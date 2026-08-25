85f9cf3cbb48b03183b53d7d06a34f9ac5f191f0
zainnh <zhaider@ucsd.edu>
2026-08-25 11:11:43 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.2
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hello"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
