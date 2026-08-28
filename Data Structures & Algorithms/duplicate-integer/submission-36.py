70de72e02902273c133283f86e340055c605f692
zainnh <zhaider@ucsd.edu>
2026-08-27 19:18:05 -0700

    Add: duplicate-integer - submission-36

---
+++ Data Structures & Algorithms/duplicate-integer/submission-36.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        new_set = set()
+        for num in nums:
+            if num in new_set:
+                return True
+            new_set.add(num)
+        return False
+          
