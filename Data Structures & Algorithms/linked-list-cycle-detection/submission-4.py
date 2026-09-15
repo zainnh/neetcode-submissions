09bd5849d53ac4e7a4edd0ef576aea94c48dfc9b
zainnh <zhaider@ucsd.edu>
2026-09-14 17:30:19 -0700

    Add: linked-list-cycle-detection - submission-4

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-4.py
@@ -0,0 +1,14 @@
+# did it with the hash map tard solution last time but reels explained the fast / slow 2 pointer approach and i guess it is more space optimal. 
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+# 19 ms faster w
