c450cbd2c18ff20e10acca945e895ce6042b41bf
zainnh <zhaider@ucsd.edu>
2026-09-23 18:18:23 -0700

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            midpoint = (left+right)//2
+            if nums[midpoint] == target:
+                return midpoint
+            elif nums[midpoint] < target:
+                left += 1
+            else:
+                right -= 1
+        return -1
+                
+
+        
